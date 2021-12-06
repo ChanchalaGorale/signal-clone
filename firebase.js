6aeb268d828220079c641fe2dab1220637a87e5e
NektorHealth <85933978+NektorHealth@users.noreply.github.com>
2021-12-06 21:26:21 +0530

    setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPCPcHeSXdC92q4Cs2cMJV3j_Hkqczxoo",
+  authDomain: "signalclone-e3712.firebaseapp.com",
+  projectId: "signalclone-e3712",
+  storageBucket: "signalclone-e3712.appspot.com",
+  messagingSenderId: "500826658338",
+  appId: "1:500826658338:web:cd1cfd74657edd879c9c7b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
